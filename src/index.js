6929ac1af9494dc8ff7e2a9f6c6f95869542d0ce
J.M <kabugijeff@gmail.com>
2023-03-27 16:09:38 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addList(e.target.querySelector("#new-task-description").value);
+    form.reset();
+  })
 });
+
+function addList(value){
+  const task = document.getElementById('tasks');
+  const el = document.createElement('li');
+  const btn = document.createElement('button');
+  btn.addEventListener('click', (e) => {
+    e.target.parentNode.remove();
+  });
+  btn.textContent = "x"
+  el.textContent = `${value} `;
+  el.appendChild(btn);
+  task.appendChild(el);
+}
